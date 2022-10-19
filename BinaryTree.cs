ca7cc17cedd2315fcc94f42500fb1040a1cd1ab2
jayukasode23 <jayukasode2398@gmail.com>
2022-10-19 19:59:35 +0530

    Added Simple Binary Search Tree

---
+++ BinaryTree.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace BinarySearchTree
+{
+    public class BinaryTree
+    {
+        public Node Root { get; set; }
+        public bool Add(int value)
+        {
+            Node befor = null, after = this.Root;
+
+            while (after != null)
+            {
+                befor = after;
+                if (value < after.Data)
+                    after = after.LeftNode;
+
+                else if (value > after.Data)
+                    after = after.RightNode;
+                else
+                {
+                    return false;
+                }
+            }
+            Node newnode = new Node();
+            newnode.Data = value;
+            if (this.Root == null)
+            {
+                this.Root = newnode;
+            }
+            else
+            {
+                if (value < befor.Data)
+                    befor.LeftNode = newnode;
+                else
+                    befor.RightNode = newnode;
+
+            }
+            return true;
+
+        }
+        public void TraversePreorder(Node Parent)
+        {
+            if (Parent != null)
+            {
+                Console.WriteLine(Parent.Data + " ");
+                TraversePreorder(Parent.LeftNode);
+                TraversePreorder(Parent.RightNode);
+
+            }
+        }
+    }
+}
